25b2c0b8e82b56cebfb6406e212f76f1adab8356
Parrot <parrot@localhost.localdomain>
2025-10-08 21:49:30 +0000

    Created Headers

--- src/parse.c
+++ src/parse.c
@@ -27,11 +27,61 @@ int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) 
 }	
 
 int validate_db_header(int fd, struct dbheader_t **headerOut) {
+    if (fd < 0) {
+        printf("Got a bad FD from the user\n");
+        return STATUS_ERROR;
+    }
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header ==-1) {
+        printf("Malloc failed to create db header\n");
+    }
+
+    if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t)){
+        perror("read");
+        free(header);
+        return STATUS_ERROR;
+    }
+    header->version = ntohs(header->version);
+    header->count = ntohs(header->count);
+    header->magic = ntohs(header->magic);
+    header->filesize = ntohs(header->filesize);
+
+    if (header->version != 1) {
+        printf("Improper header version\n");
+        free(header);
+        return -1;
+    }
+    if (header->magic != HEADER_MAGIC) {
+        printf("Improper header magic\n");
+        free(header);
+        return -1;
+    }
+
+    struct stat dbstat = {0};
+    fstat(fd, &dbstat);
+    if (header->filesize != dbstat.st_size) {
+        printf("Corrupted database\n");
+        free(header);
+        return -1;
+    }
 
 }
 
 int create_db_header(int fd, struct dbheader_t **headerOut) {
-	
+	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+
+    if (header == -1) {
+        printf("Malloc failed to create dbheader");
+        return STATUS_ERROR;
+    }
+
+    header->version = 0x1;
+    header->count = 0;
+    header->magic = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *headerOut = header;
+    return STATUS_SUCCESS;
 }
 
 
