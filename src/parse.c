a5005b7a46a2574c5294f1de0f910b534e67bf93
Parrot <parrot@localhost.localdomain>
2025-10-08 22:11:01 +0000

    Created Headers

--- src/parse.c
+++ src/parse.c
@@ -33,7 +33,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
         return STATUS_ERROR;
     }
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header ==-1) {
+    if (header == NULL) {
         printf("Malloc failed to create db header\n");
     }
 
@@ -73,7 +73,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 int create_db_header(int fd, struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
 
-    if (header == -1) {
+    if (header == NULL) {
         printf("Malloc failed to create dbheader");
         return STATUS_ERROR;
     }
